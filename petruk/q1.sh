6ec386c7daa7933f34544f40c874835d0906656c
udhaindah <udhaindah937@gmail.com>
2024-12-02 12:52:36 +0700

    Update q1.sh

--- petruk/q1.sh
+++ petruk/q1.sh
@@ -1,1 +1,4 @@
-
+rm -rf datas.txt
+wget -qO datas.txt https://raw.githubusercontent.com/udhaindah/datakadrun/refs/heads/main/datas/datas1.txt
+rm -rf proxies.txt
+wget -qO proxies.txt https://raw.githubusercontent.com/udhaindah/datakadrun/refs/heads/main/proxy/proxy1
