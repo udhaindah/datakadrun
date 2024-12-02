7fce838167090d2888c6435bf6e7048e9c0f3c03
udhaindah <udhaindah937@gmail.com>
2024-12-02 15:25:39 +0700

    Create main.sh

---
+++ cool/main.sh
@@ -0,0 +1,13 @@
+read -p "token: " TOKEN
+apt-get update > /dev/null 2>&1
+sudo apt install nodejs > /dev/null 2>&1
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash > /dev/null 2>&1
+source ~/.nvm/nvm.sh > /dev/null 2>&1
+nvm install v20.16.0 > /dev/null 2>&1
+node -v
+git clone https://github.com/udhaindah/kadrun.git
+cd kadrun
+npm i user-agents axios colors p-limit https-proxy-agent socks-proxy-agent crypto ws uuid
+npm install crypto-js
+echo "$TOKEN" > datas.txt
+while :; do timeout 300 node bot; sleep 2;echo done;echo clearing log ......; done
